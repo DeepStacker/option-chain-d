c7f6941c06263e3a123ad42c72af207429a6b33b
Shivam kumar <code.python.51@gmail.com>
2024-12-27 21:42:34 +0530

    sifted to render backend from the ec2 instance to get the ssl

--- Frontend/src/context/dataSlice.js
+++ Frontend/src/context/dataSlice.js
@@ -5,8 +5,8 @@ import { io } from 'socket.io-client';
 let socket = null; // Global variable for WebSocket connection
 
 // API configuration
-const API_BASE_URL = 'https://13.126.55.152:10001/api';
-const SOCKET_URL = 'https://13.126.55.152:5000';
+const API_BASE_URL = 'https://option-chain-d.onrender.com/api';
+const SOCKET_URL = 'https://option-chain-d-new-app.onrender.com';
 
 // Configure axios defaults
 axios.defaults.withCredentials = true;
